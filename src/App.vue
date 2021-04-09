947103d95a25c941c7184361be7dd6e809a93376
Immortal000 <kotthursiddarth@gmail.com>
2021-04-09 13:51:51 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Member playerName="Diluc"
+          imageURL="https://static1.srcdn.com/wordpress/wp-content/uploads/2020/10/diluc-genshin-impact-1.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5"
+  />
+
+</template>
+
+<script>
+import Member from "./components/player/Member"
+export default {
+  name: 'App',
+  components: {
+    Member
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+</style>
---
+++ src/components/player/Member.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="member">
+    <div class="imageholder">
+      <img :src="imageURL">
+    </div>
+    <h1>{{playerName}}</h1>
+  </div>
+  <button v-on:click="switchState">Click me</button>
+  <div v-if="show">
+    <h1>Diluc is hawt 🔥</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Member',
+  props: {
+    playerName: String,
+    imageURL: String,
+  },
+  data(){
+    return{
+      show: false
+    }
+  },
+  methods:{
+    switchState(){
+      this.show = !this.show
+    },
+  }
+}
+</script>
+
+<style>
+.member{
+  width: 300px;
+  border: 1px solid black;
+  border-radius: 15px;
+}
+
+.imageholder{
+  width: 300px;
+}
+.imageholder img{
+  width: 300px;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+}
+</style>
